513fa642d64e2de855c77a3a17d3755f9480ba33
iam-arul <iam.arul@hotmail.com>
2019-08-11 12:57:21 +0530

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import AppRouter from './AppRouter';
 import './style.css';
 
 class App extends Component {
@@ -14,10 +14,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
+        <AppRouter/>
       </div>
     );
   }
